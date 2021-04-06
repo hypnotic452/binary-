68938140fdbefec3cdb5565f98e31ff269aa3a27
hypnotic452 <82047839+hypnotic452@users.noreply.github.com>
2021-04-06 11:29:10 -0500

    Add files via upload

---
+++ binary1.0.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+print("""  
+  ____  _____   _____    ______  __   ___  ____ 
+   / __ )/  _/ | / /   |  / __ \ \/ /  <  / / __ /
+  / __  |/ //  |/ / /| | / /_/ /\  /   / / / / / /
+ / /_/ // // /|  / ___ |/ _, _/ / /   / /_/ /_/ / 
+/_____/___/_/ |_/_/  |_/_/ |_| /_/   /_/(_)____/  
+""")
+print("Creador: TheHypnotic  \nCorre@: thypnotic949@gmail.com")
+
+print("------------------------------------------------------------")
+
+print("Elija su opcion") 
+
+print("INCLUYE - ANTES DEL NUMERO, SOLO ESCRIBA EL GUION Y EL NUMERO")
+print("-------------------------------------------------------------")
+print("OPCION:  -1 Binarios")
+print("-------------------------------------------------------------")
+print("Habra mas opciones en futuras versiones")
+print("-------------------------------------------------------------")
+x = int(input("Elija su opcion: "))
+
+
+
+
+if x<0:
+      print(".0.9.0.9.0.9.9.\n0.9.0.9.0.9.0.9.0.9" * 1000)
+      print("--------------------------------------------------------")
+      print("BINARIO 2")
+      print("0.1.0.1.0.1.0.1.\n0.1.0.1.0.1.0.1.0.0." * 1000)
+
+else:
+     print("ERROR!!! intente denuevo")
+print("\nFIN DEL PROGRAMA")
+
+#Este es mi programa de noob :"v
+
+   
