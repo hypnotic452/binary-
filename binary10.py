c5598115238730c2e271bec1dc77491867eef29d
hypnotic452 <82047839+hypnotic452@users.noreply.github.com>
2021-04-07 10:21:17 -0500

    Add files via upload

---
+++ binary10.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+print("""  
+  ____  _____   _____    ______  __   ___  ____ 
+   / __ )/  _/ | / /   |  / __ \ \/ /  <  / / __ /
+  / __  |/ //  |/ / /| | / /_/ /\  /   / / / / / /
+ / /_/ // // /|  / ___ |/ _, _/ / /   / /_/ /_/ / 
+/_____/___/_/ |_/_/  |_/_/ |_| /_/   /_/(_)____/  
+""")
+print("CREADOR: TheHypnotic  \nCORRE@: thypnotic949@gmail.com")
+
+print("=======================================================")
+
+print("OPCIONES>>> 1-BiNaRiOs    2-Binary  3-Personalizado  ")
+
+print("=======================================================")
+x = int(input("===Elija su opcion=== "))
+
+
+if x==1: 
+     print("01010000100101000101001001" * 1000) 
+
+if x==2:
+     print("0.1.0.1.01.0110.01010101..10.1.0.10..10.1" * 1000)
+
+if x>2:
+    a = input("==Escriba== ")
+    print(a * 1000)
+    
+else:
+    print("ERROR!!!! INTENETE DENUEVO!!!")
+
+
+
+print("Cierre de programa.......")
+
